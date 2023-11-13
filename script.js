8e1b611ffcfb9fe85decffb32d3cf2fb63c513de
Gustavocaris <149839159+Gustavocaris@users.noreply.github.com>
2023-11-12 21:05:50 -0300

    Portfólio

---
+++ script.js
@@ -0,0 +1,8 @@
+let header =document.getElementById('header')
+
+window.addEventListener('scroll', () => {
+    if (window.scrolly ≥ 200) {
+        header.style.background = 'transparent'
+    }
+    
+})
