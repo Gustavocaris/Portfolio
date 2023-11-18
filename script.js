f02efacc6fd24a5a7666e32377a01c99891dd70d
Gustavocaris <149839159+Gustavocaris@users.noreply.github.com>
2023-11-18 19:11:05 -0300

    Portfolio

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+  });
+
+
 let header =document.getElementById('header')
 
 window.addEventListener('scroll', () => {
