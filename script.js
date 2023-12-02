2d4332aaad38be97d5fb5040a9282ca4c90812ef
Gustavocaris <149839159+Gustavocaris@users.noreply.github.com>
2023-12-02 18:00:43 -0300

    Responsividade
    alteração no 'projects'

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+
+
+
+let swiper = createSwiper (".mySwiper", ".swiper-pagination",".swiper-button-next",".swiper-button-prev")
+
+
+function createSwiper (container, pagination, nextButton, prevButton){
+  return new Swiper(container, {
+    slidesPerView: handlewidth(),
     spaceBetween: 30,
     pagination: {
-      el: ".swiper-pagination",
+      el: pagination,
       clickable: true,
     },
     navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: "nextButton",
+    prevEl: "prevButton",
     },
   });
+}
 
 
-let header =document.getElementById('header')
+
+
+let header = document.getElementById('header')
+
+function handlewidth() {
+  let getwidth = window.innerWidth || document.documentElement.clientWidth;
+  let slideshow = 3;
+
+  if (getwidth < 1001) {
+    slideshow = 2;
+  }
+
+  if (getwidth < 700) {
+    slideshow = 1;
+  }
+
+  return slideshow
+}
+
+
+window.addEventListener('resize', () => {
+  swiper.params.slidesPerView = handlewidth();
+  swiper.update();
+})
+
 
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 200) {
